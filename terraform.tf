1a3305f0b97f483229a4fb86a2c5cb691b0fd74b
Abhi.kashyap <111049350+abhi-kashyap25@users.noreply.github.com>
2024-04-21 23:26:06 +0530

    Update terraform.tf

--- terraform.tf
+++ terraform.tf
@@ -1,22 +1,30 @@
+# Terraform Cloud Configuration:
+# This block specifies the configuration for using Terraform Cloud.
 terraform {
   cloud {
-    organization = "abhi_kashyap_25"
+    # The name of your organization on Terraform Cloud.
+    organization = "your_organization"
 
+    # The name of the Terraform workspace you are using within your organization.
     workspaces {
-      name = "learn-terraform-eks"
+      name = "your_terraform_workspace"
     }
   }
 }
-  
+
+# Terraform Configuration:
+# This block specifies the requirements for Terraform, including the providers and versions.
 terraform {
+  # Specify the required providers and their versions.
   required_providers {
     aws = {
+      # The provider for AWS infrastructure.
       source  = "hashicorp/aws"
+      # Specify the version constraint for the AWS provider.
       version = "~> 5.7.0"
     }
   }
 
+  # Specify the required version of Terraform itself.
   required_version = ">= 1.3"
 }
-
-
