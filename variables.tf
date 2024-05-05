94d0de073b763816269a07220dd4302b0897f863
Abhi.kashyap <111049350+abhi-kashyap25@users.noreply.github.com>
2024-05-05 23:38:13 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -17,3 +17,18 @@ variable "secret_key" {
   description = "value for secret_key"
   
 }
+
+variable "vpc_id" {
+  description = "The ID of the VPC"
+}
+
+variable "allowed_cidr_blocks" {
+  description = "List of CIDR blocks allowed for inbound traffic"
+  type        = list(string)
+  default     = ["10.0.0.0/8", "172.16.0.0/12", "192.168.0.0/16"]
+}
+
+variable "environment" {
+  description = "The deployment environment"
+  default     = "dev"  # Set the default to 'dev' or another desired environment
+}
